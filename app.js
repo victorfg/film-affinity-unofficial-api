b231e9dba89dbae6359c8c297fcf2a42b2a3df48
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2024-03-30 20:15:14 +0100

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const filmRoutes = require('./routes/filmRoutes')
 const workersFilmRoutes = require('./routes/workerFilmRoutes')
+const path = require('path')
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 app.use(filmRoutes, workersFilmRoutes)
 
